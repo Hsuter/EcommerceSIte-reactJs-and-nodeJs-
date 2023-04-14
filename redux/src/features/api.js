2451cea79827e90fa883c5809c22b1bb9577f5c9
Harry Suter <harry.suter.hs@gmail.com>
2023-04-14 15:29:41 +0300

    .env adjustments

--- redux/src/features/api.js
+++ redux/src/features/api.js
@@ -1,4 +1,4 @@
-export const url = "http://localhost:4000/";
+export const url = "http://localhost:8000/";
 
 export const setHeaders = () => {
   const headers = {
--- redux/src/features/productsSlice.js
+++ redux/src/features/productsSlice.js
@@ -15,10 +15,10 @@ export const productsFetch = createAsyncThunk(
   async () => {
     try {
       const response = await axios.get(`${url}api/products`);
-      console.log(response.data);
+
       return response.data;
     } catch (error) {
-      console.log(error);
+      console.log("Fetching failed", error);
     }
   }
 );
